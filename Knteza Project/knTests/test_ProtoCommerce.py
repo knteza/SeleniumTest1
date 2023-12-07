35a0fc56df9fab334420c5a991da70bb9017b6d3
Kimuli <kcnteza@gmail.com>
2023-12-06 21:06:51 -0600

    # update

--- Knteza Project/knTests/test_ProtoCommerce.py
+++ Knteza Project/knTests/test_ProtoCommerce.py
@@ -6,7 +6,7 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.select import Select
-from
+
 
 @pytest.mark.usefixtures("setup")
 class TestHomePage():
